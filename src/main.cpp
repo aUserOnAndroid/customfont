d59acd82103f0b53e9b484801574b68e76ca74eb
emirontop1 <dincemr@gmail.com>
2026-05-27 17:44:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,67 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include "EmirGui.hpp"
+#include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
+// GUI Paneli Sınıfı
+class EmirMenu : public geode::Popup<std::string> {
+protected:
+    bool setup(std::string title) override {
+        this->setTitle(title);
+        auto menu = CCMenu::create();
+        this->m_mainLayer->addChild(menu);
+
+        // Noclip Toggler
+        auto noclip = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(EmirMenu::onToggle), 0.8f);
+        noclip->setPosition({-50, 20});
+        menu->addChild(noclip);
+
+        // ESP Toggler
+        auto esp = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(EmirMenu::onToggle), 0.8f);
+        esp->setPosition({-50, -20});
+        menu->addChild(esp);
+
+        return true;
+    }
+
+    void onToggle(CCObject* sender) {
+        Notification::create("Ayar Değiştirildi!", NotificationIcon::Success)->show();
+    }
+
+public:
+    static EmirMenu* create() {
+        auto ret = new EmirMenu();
+        if (ret && ret->init(300, 200, "Emir Mod - All In One", "GJ_square01.png")) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+};
+
+// Ana Menü Modifikasyonu
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
-        // Ekranda her zaman görünecek "Emir" butonu
+        auto menu = static_cast<CCMenu*>(this->getChildByID("bottom-menu"));
+        
         auto btn = CCMenuItemSpriteExtra::create(
-            ButtonSprite::create("Emir"), 
+            ButtonSprite::create("Emir Mod"), 
             this, 
             menu_selector(MyMenuLayer::onOpenEmir)
         );
         
-        auto menu = CCMenu::create();
-        menu->setPosition({50, 50}); // Ekranın sol alt köşesi
+        btn->setID("emir-btn"_spr);
         menu->addChild(btn);
-        this->addChild(menu);
+        menu->updateLayout();
 
         return true;
     }
 
     void onOpenEmir(CCObject* sender) {
-        EmirGui::EmirMenu::create()->show();
+        EmirMenu::create()->show();
     }
 };
